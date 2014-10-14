fa544d6b9d8f37b1efc36fe28e32f1b0c30e7033
Brandon Ward <ward486@regis.edu>
2014-10-14 17:53:10 -0600

    Decryption Sequence is started, I think I have a good roadmap of where I'm taking this.  Next I need to research all my fitness criteria.

--- app/src/main/java/regishonorsthesis/brandonward/cryptogeneticalgorithmthesis/business/DecryptionManager.java
+++ app/src/main/java/regishonorsthesis/brandonward/cryptogeneticalgorithmthesis/business/DecryptionManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import regishonorsthesis.brandonward.cryptogeneticalgorithmthesis.domain.Ciphertext;
 import regishonorsthesis.brandonward.cryptogeneticalgorithmthesis.domain.Decryption;
 import regishonorsthesis.brandonward.cryptogeneticalgorithmthesis.domain.Gene;
+import regishonorsthesis.brandonward.cryptogeneticalgorithmthesis.domain.Position;
 
 /**
  * Created by BrandonWard on 10/13/2014.
@@ -15,6 +16,7 @@ public class DecryptionManager {//The Manager needs to keep track of the Ciphert
 
     private Ciphertext cipher;
     private Decryption decryption;
+    private boolean[] locked = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};//26 falses, because at the start, all letters can be swapped.
     private double[] singleLetterFrequencies =
             //a    b    c    d     e    f    g    h    i    j    k    l    m    n    o    p    q    r    s    t    u    v    w    x    y    z
             {8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1};
@@ -40,7 +42,34 @@ public class DecryptionManager {//The Manager needs to keep track of the Ciphert
     public void nextDecryption() {
         double prevFitness = decryption.getFitness();//Save previous fitness and decryption
         Decryption prevDecrypt = decryption;//Test after changing decryption to see if new iteration should be kept
+        decryption = newDecryption();
+        calculateFitness();
+        //TODO: calculate decryption
+        //TODO: calculate which positions need to be locked
+    }
 
+    public Decryption newDecryption() {
+        //Split into arrays of kept and unkept bits
+        List<Character> shuffle = decryption.getGene().getGene();
+        int j = 0;//track index of lock
+        int k = 0;//track index of unlock
+        List<Position> lock = new ArrayList<Position>();
+        List<Position> unlock = new ArrayList<Position>();
+        for (int i = 0; i < locked.length; i++) {
+            Position position = new Position();
+            position.setPosition(i);
+            position.setCharacter(shuffle.get(i));
+            if (locked[i]) {
+                lock.add(position);
+            } else {
+                unlock.add(position);
+            }
+            //TODO: Shuffle unlocked positions
+            //TODO: put unlocked positions back into list where locked == false
+            //TODO: put locked positions back into list where locked == true and position == position
+            //TODO: assign this Gene to a new Decryption and return the Decryption to have fitness calculated.
+        }
+        return null;
     }
 
     public void calculateFitness() {
---
+++ app/src/main/java/regishonorsthesis/brandonward/cryptogeneticalgorithmthesis/domain/Position.java
@@ -0,0 +1,26 @@
+package regishonorsthesis.brandonward.cryptogeneticalgorithmthesis.domain;
+
+/**
+ * Created by BrandonWard on 10/14/2014.
+ */
+public class Position {
+
+    private char character;
+    private int position;
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
